3182651ce4c96e85c528b1db4040d3308ba96785
Julio Jaramillo <theremsoe@gmail.com>
2023-02-14 14:25:12 -0600

    feat: added support for collectd skupos integration

--- app/index.js
+++ app/index.js
@@ -1,17 +1,17 @@
 const http = require('http');
-const SDC = require('statsd-client'), sdc = new SDC({host: 'statsd.example.com'});
+const SDC = require('statsd-client'), sdc = new SDC({host: '127.0.0.1'});
 
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
         const cpu  = Math.random()
-        sdc.raw(`system.cpu:${cpu}|g`)
+        returned_value = sdc.raw(`system.cpu:${cpu.toFixed('3')}|g`)
+        console.log(returned_value)
         res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-        res.write('<html><body><p>Random value was stored.</p></body></html>');
+        res.write('<html><body><p>Random value was stored as a metric value.</p></body></html>');
         res.end();
     }
     else
         res.end('Invalid Request!');
-
 });
 
 server.listen(8000);
--- dockerfiles/node-js-server.dockerfile
+++ dockerfiles/node-js-server.dockerfile
@@ -2,10 +2,16 @@ FROM node:16
 
 WORKDIR /var/www/html
 
-COPY --chown=www-data:www-data . /var/www/html/
+COPY --chown=www-data:www-data ./app /var/www/html/
 
-RUN apt-get install collectd
+RUN apt-get update -y
+RUN apt-get install collectd -y
+RUN apt-get clean --dry-run
+
+COPY ./conf/collectd.conf /etc/collectd/collectd.conf
+
+RUN service collectd start
 
 USER www-data
 
-CMD node app/index.js
+CMD node index.js
